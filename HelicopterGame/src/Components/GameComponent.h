9b59f6b21a2976017bddbcc1492e60c42524f995
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2024-01-22 14:11:24 +0200

    Added virtual Init() function into GameComponent for adding pointer to Renderer as needed

--- HelicopterGame/src/Components/GameComponent.h
+++ HelicopterGame/src/Components/GameComponent.h
@@ -1,3 +1,6 @@
+#ifndef GAME_COMPONENT_H
+#define GAME_COMPONENT_H
+
 #include <string>
 
 // Base class for components in game
@@ -6,11 +9,14 @@ class GameComponent
 public:
 	GameComponent() = default;
 
-	virtual void Update();
+	// Initialize component with reference to renderer
+	virtual void Init(class Renderer*) = 0;
+	virtual void Update() = 0;
 
 	std::string GetName() const { return m_Name; }
 
-	~GameComponent();
 protected:
 	std::string m_Name;
 };
+
+#endif // !GAME_COMPONENT_H
